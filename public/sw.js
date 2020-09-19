2d42b18f080761a7b6180bc7b3ab8568e7f70e0b
ShahzaibSE <shahzaibanalyst@outlook.com>
2020-09-19 21:09:28 +0500

    Setting up push notification.

--- public/sw.js
+++ public/sw.js
@@ -14,6 +14,19 @@ let urlsToCache = [
 
 console.log("Service Worker started: ",cache_name)
 
+this.importScripts("https://www.gstatic.com/firebasejs/7.20.0/firebase-app.js")
+this.importScripts("https://www.gstatic.com/firebasejs/7.20.0/firebase-messaging.js")
+
+this.firebase.initializeApp({
+  apiKey: "AIzaSyBQjQc0CUWse4I4Dc7Alrl7wodBMzUaoLA",
+  authDomain: "fir-messaging-8ed83.firebaseapp.com",
+  databaseURL: "https://fir-messaging-8ed83.firebaseio.com",
+  projectId: "fir-messaging-8ed83",
+  storageBucket: "fir-messaging-8ed83.appspot.com",
+  messagingSenderId: "942472815958",
+  appId: "1:942472815958:web:8bfc18803a8d555119a2f7"
+})
+
 this.addEventListener('install', (event)=>{
     event.waitUntil(
         caches.open(cache_name).then(cache => {
@@ -37,4 +50,6 @@ this.addEventListener('fetch', function(event) {
         }
       )
     );
-  })
+})
+
+this.firebase.messaging()
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { getTokenSourceMapRange } from 'typescript';
 import './App.css';
 // Component.
 import GridComponent from "./components/Grid/Grid"
-
+// Firebase configuration.
+import firebase from "./firebase"
 function App() {
+  const messaging = firebase.messaging()
+  Notification.requestPermission().then(() =>{
+    return messaging.getToken()
+  }).then(token => {
+    console.log("Token")
+    console.log(token)
+  })
+
   return (
     <div>
       <GridComponent/>
